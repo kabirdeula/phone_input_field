1ae105f533527da0ef50e7db7f8999a88926eb35
Kabir Deula <kabirdeula167@gmail.com>
2026-04-09 17:02:11 +0545

    feat: add countries data

---
+++ lib/src/data/countries_data.dart
@@ -0,0 +1,124 @@
+import '../models/country_model.dart';
+
+final List<Country> countriesData = [
+  Country(
+    name: 'Indonesia',
+    countryCode: 'ID',
+    dialCode: '+62',
+    flag: '🇮🇩',
+  ),
+  Country(
+    name: 'United States',
+    countryCode: 'US',
+    dialCode: '+1',
+    flag: '🇺🇸',
+  ),
+  Country(
+    name: 'United Kingdom',
+    countryCode: 'GB',
+    dialCode: '+44',
+    flag: '🇬🇧',
+  ),
+  Country(
+    name: 'Canada',
+    countryCode: 'CA',
+    dialCode: '+1',
+    flag: '🇨🇦',
+  ),
+  Country(
+    name: 'Australia',
+    countryCode: 'AU',
+    dialCode: '+61',
+    flag: '🇦🇺',
+  ),
+  Country(
+    name: 'Nepal',
+    countryCode: 'NP',
+    dialCode: '+977',
+    flag: '🇳🇵',
+  ),
+  Country(
+    name: 'India',
+    countryCode: 'IN',
+    dialCode: '+91',
+    flag: '🇮🇳',
+  ),
+  Country(
+    name: 'Bangladesh',
+    countryCode: 'BD',
+    dialCode: '+880',
+    flag: '🇧🇩',
+  ),
+  Country(
+    name: 'Pakistan',
+    countryCode: 'PK',
+    dialCode: '+92',
+    flag: '🇵🇰',
+  ),
+  Country(
+    name: 'Thailand',
+    countryCode: 'TH',
+    dialCode: '+66',
+    flag: '🇹🇭',
+  ),
+  Country(
+    name: 'Vietnam',
+    countryCode: 'VN',
+    dialCode: '+84',
+    flag: '🇻🇳',
+  ),
+  Country(
+    name: 'Malaysia',
+    countryCode: 'MY',
+    dialCode: '+60',
+    flag: '🇲🇾',
+  ),
+  Country(
+    name: 'Singapore',
+    countryCode: 'SG',
+    dialCode: '+65',
+    flag: '🇸🇬',
+  ),
+  Country(
+    name: 'Philippines',
+    countryCode: 'PH',
+    dialCode: '+63',
+    flag: '🇵🇭',
+  ),
+  Country(
+    name: 'Japan',
+    countryCode: 'JP',
+    dialCode: '+81',
+    flag: '🇯🇵',
+  ),
+  Country(
+    name: 'South Korea',
+    countryCode: 'KR',
+    dialCode: '+82',
+    flag: '🇰🇷',
+  ),
+  Country(
+    name: 'China',
+    countryCode: 'CN',
+    dialCode: '+86',
+    flag: '🇨🇳',
+  ),
+  Country(
+    name: 'Germany',
+    countryCode: 'DE',
+    dialCode: '+49',
+    flag: '🇩🇪',
+  ),
+  Country(
+    name: 'France',
+    countryCode: 'FR',
+    dialCode: '+33',
+    flag: '🇫🇷',
+  ),
+  Country(
+    name: 'Italy',
+    countryCode: 'IT',
+    dialCode: '+39',
+    flag: '🇮🇹',
+  ),
+];
